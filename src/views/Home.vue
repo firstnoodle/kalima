81798ef0819075c067afb095711bd2f4e315baf4
sebastian thielke <seb@firstnoodle.io>
2021-03-22 23:17:09 +0100

    Home: removed vue logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+    <div class="home">
+        Kalima
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: "Home",
-  components: {
-  }
+    name: 'Home',
 });
 </script>
