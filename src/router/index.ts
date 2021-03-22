a13712405509ba95454a68507f1152df5334363f
sebastian thielke <seb@firstnoodle.io>
2021-03-22 23:17:38 +0100

    router cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
 import Alphabet from '../views/Alphabet.vue';
-import Character from '../views/Character.vue';
 
 Vue.use(VueRouter);
 
@@ -17,27 +16,13 @@ const routes: Array<RouteConfig> = [
         name: 'Alphabet',
         component: Alphabet,
     },
-    {
-        path: '/:id',
-        name: 'Character',
-        component: Character,
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    },
 ];
 
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
-    linkActiveClass: 'z-10 shadow-lg border-gray-400 opacity-100',
+    linkActiveClass: 'bg-gray-100',
 });
 
 export default router;
